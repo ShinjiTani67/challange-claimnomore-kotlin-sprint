81648af915af34b25bb4c47a887f441049818cdc
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2024-10-04 14:29:07 -0300

    add de agenda e notafiscal activities

---
+++ app/src/main/java/com/example/challange_sprint01_claimnomore/Agenda.kt
@@ -0,0 +1,20 @@
+package com.example.challange_sprint01_claimnomore
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Agenda : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_agenda)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
