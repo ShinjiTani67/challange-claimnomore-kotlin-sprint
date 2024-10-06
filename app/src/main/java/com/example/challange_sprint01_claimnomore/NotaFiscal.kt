1b5a7adc641d94f8c2b60b91dc81e6bcbb2c8494
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2024-10-06 17:26:47 -0300

    add de edit text

--- app/src/main/java/com/example/challange_sprint01_claimnomore/NotaFiscal.kt
+++ app/src/main/java/com/example/challange_sprint01_claimnomore/NotaFiscal.kt
@@ -1,6 +1,7 @@
 package com.example.challange_sprint01_claimnomore
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
