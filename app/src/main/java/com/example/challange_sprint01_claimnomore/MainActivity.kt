2e3dc45da68d93f1e7cebe55e1f5965d31ef7d57
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2024-10-06 23:02:36 -0300

    add de botao

--- app/src/main/java/com/example/challange_sprint01_claimnomore/MainActivity.kt
+++ app/src/main/java/com/example/challange_sprint01_claimnomore/MainActivity.kt
@@ -7,6 +7,17 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button_open_second.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
