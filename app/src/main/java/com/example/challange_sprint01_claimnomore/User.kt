bf658c8e21e85ace3adea61606b3a8f9c769272f
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2024-10-07 00:10:09 -0300

    update user

--- app/src/main/java/com/example/challange_sprint01_claimnomore/User.kt
+++ app/src/main/java/com/example/challange_sprint01_claimnomore/User.kt
@@ -1,6 +1,7 @@
 package com.example.challange_sprint01_claimnomore
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +10,12 @@ import androidx.core.view.WindowInsetsCompat
 class User : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_user)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val textViewDisplay = findViewById<TextView>(R.id.textViewDisplay)
+
+        val inputText = intent.getStringExtra("inputText")
+        textViewDisplay.text = inputText ?: "Nada recebido"
         }
     }
 }
