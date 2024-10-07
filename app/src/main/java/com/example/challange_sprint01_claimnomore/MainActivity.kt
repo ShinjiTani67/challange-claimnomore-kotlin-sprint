2147ab314bf43d7da71c1cee4f0ff4937690a936
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2024-10-07 00:08:35 -0300

    update main

--- app/src/main/java/com/example/challange_sprint01_claimnomore/MainActivity.kt
+++ app/src/main/java/com/example/challange_sprint01_claimnomore/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.challange_sprint01_claimnomore
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -13,20 +15,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        entrarfuncionario.setOnClickListener {
-            val intent = Intent(this, activity_user::class.java)
-            startActivity(intent)
-        }
-    }
+        val editTextInput = findViewById<EditText>(R.id.editTextInput)
+        val buttonNavigate = findViewById<Button>(R.id.entrarfuncionario)
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+    }
+    buttonNavigate.setOnClickListener {
+        val inputText = editTextInput.text.toString()
+        val intent = Intent(this, User::class.java)
+        intent.putExtra("inputText", inputText)
+        startActivity(intent)
+
     }
 }
