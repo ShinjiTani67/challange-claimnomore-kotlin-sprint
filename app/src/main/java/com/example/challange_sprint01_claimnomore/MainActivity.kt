c327618ae29541f312f417de720a63d0e3ac1a4c
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2024-11-05 14:53:05 -0300

    update da main activity

--- app/src/main/java/com/example/challange_sprint01_claimnomore/MainActivity.kt
+++ app/src/main/java/com/example/challange_sprint01_claimnomore/MainActivity.kt
@@ -3,31 +3,31 @@ package com.example.challange_sprint01_claimnomore
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.appcompat.app.AppCompatActivity
 
 
-class MainActivity : ComponentActivity() {
+class MainActivity :  AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val entrarFuncionarioBtn: Button = findViewById(R.id.entrarFuncionarioBtn)
-        entrarFuncionarioBtn.setOnClickListener() {
+        val usuarioEditText = findViewById<EditText>(R.id.usuario)
+        val senhaEditText = findViewById<EditText>(R.id.senha)
+        val entrarFuncionarioBtnButton = findViewById<Button>(R.id.entrarFuncionarioBtn) //somente para teste
+        val entrarDentistaBtnButton: Button = findViewById(R.id.entrarDentistaBtn) //somente para teste
+
+
+        entrarFuncionarioBtnButton.setOnClickListener() {
             val intent = Intent(this, UsuarioFragment::class.java)
             startActivity(intent)
-
-            val entrarDentistaBtn: Button = findViewById(R.id.entrarDentistaBtn)
-            entrarDentistaBtn.setOnClickListener() {
-                val intent = Intent(this, UsuarioFragment   ::class.java)
-                startActivity(intent)
             }
+        entrarDentistaBtnButton.setOnClickListener(){
+            val intent = Intent(this, DentistaFragment::class.java)
+            startActivity(intent)
+        }
         }
     }
-
-
-
-
-}
