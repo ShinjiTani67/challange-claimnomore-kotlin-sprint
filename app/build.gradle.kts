4276fbb65f114afde8a47895d2a6afa7bf48e548
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2024-11-06 10:01:39 -0300

    update graddle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,8 +51,4 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation(androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1)
-    implementation(androidx.lifecycle:lifecycle-livedata-ktx:2.6.1)
-    implementation(androidx.room:room-runtime:2.6.0)
-    kapt(androidx.room:room-compiler:2.6.0)
 }
