f8bc4635b4ad11c1a6cd399b40bb051671cca8f8
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2024-11-07 23:04:26 -0300

    update geral

--- app/src/main/java/com/example/challange_sprint01_claimnomore/MainActivity.kt
+++ app/src/main/java/com/example/challange_sprint01_claimnomore/MainActivity.kt
@@ -1,35 +1,22 @@
 package com.example.challange_sprint01_claimnomore
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.auth.FirebaseAuth
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
 
 
 class MainActivity :  AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-
-
-
-        val usuarioEditText = findViewById<EditText>(R.id.usuario)
-        val senhaEditText = findViewById<EditText>(R.id.senha)
-        val entrarFuncionarioBtnButton = findViewById<Button>(R.id.entrarFuncionarioBtn) //somente para teste
-        val entrarDentistaBtnButton: Button = findViewById(R.id.entrarDentistaBtn) //somente para teste
-
-
-        entrarFuncionarioBtnButton.setOnClickListener() {
-            val intent = Intent(this, UsuarioFragment::class.java)
-            startActivity(intent)
-            }
-        entrarDentistaBtnButton.setOnClickListener(){
-            val intent = Intent(this, DentistaFragment::class.java)
-            startActivity(intent)
-        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
         }
     }
+}
